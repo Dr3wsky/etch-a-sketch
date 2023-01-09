631e3b4ad4e09e176d02877696696242adbea5c2
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-01-09 12:55:03 -0700

    starting on html blocking structure

---
+++ etch-a-sketch-func.js
@@ -0,0 +1,10 @@
+const pixel = document.createElement('div');
+const num = 64;
+const screenWidth = document.
+
+pixel.id = "pixel";
+
+function makeScreen(num) {
+    for (i = 0; i < num; i++)
+        dim = 
+}
