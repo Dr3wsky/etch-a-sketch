919210f3188ae84b299e7d26ddf97d1a1c94f11b
Andrew McFadden <mcfadden.andrewj@gmail.com>
2023-01-11 11:52:44 -0700

    fixed active button bug

--- etch-a-sketch-func.js
+++ etch-a-sketch-func.js
@@ -1,24 +1,23 @@
 /* Initialize default values and DOM manipulations */
-const DEFAULT_COLOR = 'black';
-const DEFAULT_MODE = 'color';
+const DEFAULT_COLOR = "black";
+const DEFAULT_MODE = "color";
 const DEFAULT_SIZE = 16;
 
 let activeColor = DEFAULT_COLOR;
 let activeSize = DEFAULT_SIZE;
 let activeMode = DEFAULT_MODE;
 
-
-const colorPicker = document.getElementById('colorPicker');
-const colorBtn = document.getElementById('colorBtn');
-const rainbowBtn = document.getElementById('rainbowBtn');
-const eraserBtn = document.getElementById('eraserBtn');
-const clearBtn = document.getElementById('clearBtn');
-const sizeValue = document.getElementById('sizeValue');
-const sizeSlider = document.getElementById('sizeSlider');
-const screen = document.getElementById('screen');
+const colorPicker = document.getElementById("colorPicker");
+const colorBtn = document.getElementById("colorBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const clearBtn = document.getElementById("clearBtn");
+const sizeValue = document.getElementById("sizeValue");
+const sizeSlider = document.getElementById("sizeSlider");
+const screen = document.getElementById("screen");
 
 window.onload = () => {
-    makePixels(DEFAULT_SIZE);
+  makePixels(DEFAULT_SIZE);
 };
 
 /* Add events to settings */
@@ -30,72 +29,75 @@ window.onmouseup = () => (mouseDown = false);
 
 clearBtn.onclick = () => resetScreen();
 colorPicker.oninput = (e) => setColor(e.target.value);
-rainbowBtn.onclick = () => setMode('rainbow'); 
-
-
+colorBtn.onclick = () => setMode("color");
+rainbowBtn.onclick = () => setMode("rainbow");
+eraserBtn.onclick = () => setMode("eraser");
 
 /* Define working functions */
 function clearScreen() {
-    screen.innerHTML = '';
-};
+  screen.innerHTML = "";
+}
 
 function resetScreen() {
-    clearScreen();
-    makePixels(activeSize);
-};
+  clearScreen();
+  makePixels(activeSize);
+}
 
 function setColor(newColor) {
-    activeColor = newColor;
-};
+  activeColor = newColor;
+}
 
 function setMode(newMode) {
-    activeMode = newMode;
-    setAvctiveButton(newMode);
-};
-
+  setActiveButton(newMode);
+  activeMode = newMode;
+}
 
 function makePixels(size) {
-    screen.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    screen.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  screen.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  screen.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    for (let i = 0; i < size *size; i++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixel.addEventListener('mouseover', draw);
-        pixel.addEventListener('mousedown', draw);
-        screen.appendChild(pixel);
-    };
-};
+  for (let i = 0; i < size * size; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.addEventListener("mouseover", draw);
+    pixel.addEventListener("mousedown", draw);
+    screen.appendChild(pixel);
+  }
+}
 
 function draw(e) {
-    if (e.type == 'mouseover' && !mouseDown) return;
-    if (activeMode == 'color') {
-        e.target.style.backgroundColor = activeColor;
-        e.target.style.border = activeColor;
-    };
-    if (activeMode == 'rainbow') {
-        const randomR = Math.floor(Math.random() * 256);
-        const randomG = Math.floor(Math.random() * 256);
-        const randomB = Math.floor(Math.random() * 256);
-        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-        e.target.style.border = `rgb(${randomR}, ${randomG}, ${randomB})`;
-    };
-};
-
-function setActiveButton(updatedMode) {
-    if (currentMode === 'rainbow') {
-      rainbowBtn.classList.remove('active')
-    } else if (currentMode === 'color') {
-      colorBtn.classList.remove('active')
-    } else if (currentMode === 'eraser') {
-      eraserBtn.classList.remove('active')
-    }
-  
-    if (newMode === 'rainbow') {
-      rainbowBtn.classList.add('active')
-    } else if (newMode === 'color') {
-      colorBtn.classList.add('active')
-    } else if (newMode === 'eraser') {
-      eraserBtn.classList.add('active')
-    }
+  if (e.type === "mouseover" && !mouseDown) return;
+  if (activeMode === "color") {
+    e.target.style.backgroundColor = activeColor;
+    e.target.style.border = activeColor;
   }
+  if (activeMode === "rainbow") {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    e.target.style.border = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  }
+  if (activeMode === "eraser") {
+    e.target.style.backgroundColor = "white";
+    e.target.style.border = 'var(--primary-light) 1px solid';
+  }
+}
+
+function setActiveButton(newMode) {
+  if (activeMode === "rainbow") {
+    rainbowBtn.classList.remove("active");
+  } else if (activeMode === "color") {
+    colorBtn.classList.remove("active");
+  } else if (activeMode === "eraser") {
+    eraserBtn.classList.remove("active");
+  }
+
+  if (newMode === "rainbow") {
+    rainbowBtn.classList.add("active");
+  } else if (newMode === "color") {
+    colorBtn.classList.add("active");
+  } else if (newMode === "eraser") {
+    eraserBtn.classList.add("active");
+  }
+}
